41dd3473daf5f73bb13abe213d20f01f57acd184
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-03-16 22:40:57 +0200

    - added cache settings step

---
+++ src/main/webapp/app/entities/service/wizard-dialog/cache-wizard-step.component.js
@@ -0,0 +1,31 @@
+(function() {
+    "use strict";
+
+    angular.module("flairbiApp").component("cacheWizardStep", {
+        templateUrl:
+            "app/entities/service/wizard-dialog/cache-wizard-step.component.html",
+        controller: CacheWizardStepController,
+        controllerAs: "vm",
+        bindings: {
+            connectionProperties: "=",
+        }
+    });
+
+    CacheWizardStepController.$inject = ["$scope","$rootScope"];
+    function CacheWizardStepController($scope,$rootScope) {
+        var vm = this;
+        vm.nextStep=nextStep;
+        vm.previousStep=previousStep;
+        vm.$onInit = function() {};
+        vm.$onChanges = function(changesObj) {};
+        vm.$onDestroy = function() {};
+
+        function nextStep(){
+            $rootScope.$broadcast('flairbiApp:data-connection:next-page');
+        }
+
+        function previousStep(){
+            $rootScope.$broadcast('flairbiApp:data-connection:previous-page');
+        }
+    }
+})();

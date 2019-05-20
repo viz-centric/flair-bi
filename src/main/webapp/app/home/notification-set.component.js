293f07a74b33e8760b8f7541a3d49af9048f48fa
khushbumwa <khushbum.wa@gmail.com>
2019-05-20 20:46:15 +0530

    notification_task

--- src/main/webapp/app/home/notification-set.component.js
+++ src/main/webapp/app/home/notification-set.component.js
@@ -9,9 +9,9 @@
             controllerAs: 'vm'
         });
 
-    notificationSetController.$inject = ['$scope', '$state', 'alertsService', 'stompClientService', 'AuthServerProvider', 'schedulerService'];
+    notificationSetController.$inject = ['$scope', '$state', 'alertsService', 'stompClientService', 'AuthServerProvider', 'schedulerService', 'Visualmetadata', 'VisualizationUtils', 'D3Utils'];
 
-    function notificationSetController($scope, $state, alertsService, stompClientService, AuthServerProvider, schedulerService) {
+    function notificationSetController($scope, $state, alertsService, stompClientService, AuthServerProvider, schedulerService, Visualmetadata, VisualizationUtils, D3Utils) {
         var vm = this;
         vm.pageSize = 5;
         vm.setPage = setPage;
@@ -21,209 +21,22 @@
         vm.noOfPages = 1;
         vm.currentPage = 0;
         vm.count = 0;
-        vm.reports;
+        vm.pagedItems = [];
+        vm.visualmetadata = [];
 
         active();
 
         function active() {
             // vm.alerts=vm.releaseAlert.alerts;
             // vm.count=vm.releaseAlert.count;
+            vm.pagedItems = []
             connectWebSocket();
             getScheduledReportsCount();
-            vm.reports=[
-                {
-                    "report": {
-                        "userid": "flairadmin",
-                        "connection_name": "Ecommerce",
-                        "source_id": "1715917d-fff8-44a1-af02-ee2cd41a3609",
-                        "mail_body": "This is a test email to check api functionality",
-                        "subject": "Report : Pie Chart : Tue May 14 08:55:04 IST 2019",
-                        "report_name": "Pie-Chart-f896fbc01af901edec520880b30018f5--47b5f320-bebe-4da2-884e-6c304e4bc11e",
-                        "title_name": "Pie Chart"
-                    },
-                    "report_line_item": {
-                        "visualizationid": "6b6439ed7bc10719a8cab6427b0009af--62038af8-4834-457c-8786-724a4982aea0",
-                        "dimension": [
-                            "order_item_id"
-                        ],
-                        "measure": [
-                            "product_price"
-                        ],
-                        "visualization": "Pie Chart"
-                    },
-                    "assign_report": {
-                        "channel": "email",
-                        "slack_API_Token": null,
-                        "channel_id": null,
-                        "stride_API_Token": null,
-                        "stride_cloud_id": null,
-                        "stride_conversation_id": null,
-                        "condition": "test",
-                        "email_list": [
-                            {
-                                "user_email": "flairadmin@localhost",
-                                "user_name": "Administrator Administrator"
-                            }
-                        ]
-                    },
-                    "schedule": {
-                        "cron_exp": "*/1 * * * *",
-                        "timezone": "",
-                        "start_date": "2019-05-12T18:30:00.000Z",
-                        "end_date": "2019-05-25T18:30:00.000Z"
-                    },
-                    "query": "{\"queryId\":\"f896fbc01af901edec520880b30018f5--47b5f320-bebe-4da2-884e-6c304e4bc11e\",\"userId\":\"flairadmin\",\"sourceId\":\"1715917d-fff8-44a1-af02-ee2cd41a3609\",\"source\":\"Ecommerce\",\"fields\":[\"order_item_id\",\"COUNT(product_price) as product_price\"],\"groupBy\":[\"order_item_id\"],\"limit\":\"20\",\"conditionExpressions\":[{\"sourceType\":\"FILTER\",\"expressionType\":\"CONTAINS\",\"conditionExpression\":\"{\\\"values\\\":[\\\"1\\\",\\\"2\\\",\\\"3\\\",\\\"4\\\",\\\"5\\\",\\\"6\\\",\\\"7\\\"],\\\"featureName\\\":\\\"order_item_id\\\",\\\"uuid\\\":\\\"a7d48606-6665-49bf-bc51-be217f4250ef\\\"}\",\"andOrExpressionType\":{}}]}"
-                },
-                {
-                    "report": {
-                        "userid": "flairadmin",
-                        "connection_name": "Sales",
-                        "source_id": "1715917d-fff8-44a1-af02-ee2cd41a3609",
-                        "mail_body": "This is a test email to check api functionality",
-                        "subject": "Report : Clustered Vertical Bar Chart : Sat May 18 13:33:30 IST 2019",
-                        "report_name": "Clustered-Vertical-Bar-Chart-e98586e122bff1756c53ade348000f4f--30e832c5-0ade-41ee-ab80-4065a3af283e",
-                        "title_name": "Clustered Vertical Bar Chart"
-                    },
-                    "report_line_item": {
-                        "visualizationid": "e98586e122bff1756c53ade348000f4f--30e832c5-0ade-41ee-ab80-4065a3af283e",
-                        "dimension": [
-                            "Transaction_date"
-                        ],
-                        "measure": [
-                            "Price"
-                        ],
-                        "visualization": "Clustered Vertical Bar Chart"
-                    },
-                    "assign_report": {
-                        "channel": "email",
-                        "slack_API_Token": null,
-                        "channel_id": null,
-                        "stride_API_Token": null,
-                        "stride_cloud_id": null,
-                        "stride_conversation_id": null,
-                        "condition": "test",
-                        "email_list": [
-                            {
-                                "user_email": "flairadmin@localhost",
-                                "user_name": "Administrator Administrator"
-                            }
-                        ]
-                    },
-                    "schedule": {
-                        "cron_exp": "*/1 * * * *",
-                        "timezone": "",
-                        "start_date": "2019-05-13T18:30:00.000Z",
-                        "end_date": "2019-05-22T18:30:00.000Z"
-                    },
-                    "query": "{\"queryId\":\"e98586e122bff1756c53ade348000f4f--30e832c5-0ade-41ee-ab80-4065a3af283e\",\"userId\":\"flairadmin\",\"sourceId\":\"1715917d-fff8-44a1-af02-ee2cd41a3609\",\"source\":\"Sales\",\"fields\":[\"Transaction_date\",\"COUNT(Price) as Price\"],\"groupBy\":[\"Transaction_date\"],\"limit\":\"20\"}"
-                }
-            ];//getScheduleReports(10,0)
-
-            var data = [{
-                city: 'Kathmandu',
-                area: 120,
-                population: 200
-            }, {
-                city: 'Delhi',
-                area: 90,
-                population: 360
-            }, {
-                city: 'Detroit',
-                area: 310,
-                population: 47
-            }, {
-                city: 'London',
-                area: 140,
-                population: 440
-            }
-            ];
-
-            var config = {
-                dimension: ['city'],
-                measure: ['area', 'population'],
-                showLegend: false, // true|false
-                legendPosition: 'bottom', // top|bottom|right|left
-                "showXaxis": false,
-                "showXaxisLabel": false,
-                "showYaxis": false,
-                "showXaxisLabel": false,
-                "showYaxisLabel": false,
-                "xAxisColor": "#ff0000",
-                "yAxisColor": "#00ff00",
-                "showGrid": true,
-                "stacked": false,
-                "displayName": "Dimension 1",
-                "showValues": [true, true],
-                "displayNameForMeasure": ["Measure 1", "Measure 2"],
-                "fontStyle": ["italic", "bold"],
-                "fontWeight": ["bold", "900"],
-                "numberFormat": ["Actual", "Actual"],
-                "textColor": ["#e06a6a", "#00ff00"],
-                "displayColor": ["", ""],
-                "borderColor": ["", ""],
-                "fontSize": ["9", "9"]
-            };
-
-            var ClusteredVertical = d3.select("#ClusteredVertical");
-
-            var clusteredverticalbar = flairVisualizations.clusteredverticalbar()
-                .config(config)
-                .tooltip(true)
-                .print(false);
-
-            ClusteredVertical.datum(data)
-                .call(clusteredverticalbar);
-
-
-            var ClusteredHorizontal = d3.select("#ClusteredHorizontal");
-
-            var div = d3.select(ClusteredHorizontal.node().parentNode)
-            var tooltip = div.append('div')
-                .attr('class', 'custom_tooltip');
-
-            var clusteredhorizontalbar = flairVisualizations.clusteredhorizontalbar()
-                .config(config)
-                .tooltip(true)
-                .print(true);
-
-            ClusteredHorizontal.datum(data)
-                .call(clusteredhorizontalbar);
-
-
-
-            var StackedVertical = d3.select("#StackedVertical");
-
-            var div = d3.select(StackedVertical.node().parentNode)
-            var tooltip = div.append('div')
-                .attr('class', 'custom_tooltip');
-
-            var stackedverticalbar = flairVisualizations.stackedverticalbar()
-                .config(config)
-                .tooltip(true)
-                .print(true);
-
-            StackedVertical.datum(data)
-                .call(stackedverticalbar);
-
-
-            var StackedHorizontal = d3.select("#StackedHorizontal");
-
-            var div = d3.select(StackedHorizontal.node().parentNode)
-            var tooltip = div.append('div')
-                .attr('class', 'custom_tooltip');
-
-            var stackedhorizontalbar = flairVisualizations.stackedhorizontalbar()
-                .config(config)
-                .tooltip(true)
-                .print(false)
-                .notification(true);
-
-            StackedHorizontal.datum(data)
-                .call(stackedhorizontalbar);
         }
 
 
         function getScheduleReports(pageSize, page) {
+            vm.pagedItems = [];
             schedulerService.getScheduleReports(pageSize, page);
         }
 
@@ -266,6 +79,7 @@
             stompClientService.connect(
                 { token: AuthServerProvider.getToken() },
                 function (frame) {
+                    vm.pagedItems = [];
                     console.log('notificationSetController connected web socket');
                     stompClientService.subscribe("/user/exchange/scheduledReports", onExchangeMetadata);
                     stompClientService.subscribe("/user/exchange/metaDataError", onExchangeMetadataError);
@@ -274,6 +88,7 @@
 
             $scope.$on("$destroy", function (event) {
                 console.log('flair-bi controller destorying web socket');
+                vm.pagedItems = [];
                 stompClientService.disconnect();
             });
         }
@@ -285,6 +100,120 @@
         function onExchangeMetadata(data) {
             console.log('notificationSetController on metadata', data);
             var metaData = JSON.parse(data.body);
+
+            Visualmetadata.get({
+                id: metaData.report_line_item.visualizationid
+
+            }).
+                $promise.then(
+                    function (result) {
+
+                        vm.visualmetadata.push(result);
+                        var obj = {}
+                        obj.data = JSON.parse(metaData.queryResponse).data;
+                        obj.title = metaData.report.title_name;
+                        obj.property = result.properties;
+                        obj.visualization = metaData.report_line_item.visualization;
+                        obj.visualizationid = metaData.report_line_item.visualizationid;
+                        var features = VisualizationUtils.getDimensionsAndMeasuresForNotification(result.fields),
+                            dimensions = features.dimensions,
+                            measures = features.measures,
+                            colorSet = D3Utils.getDefaultColorset();
+                        var result = {};
+                        if (obj.visualization == "Clustered Vertical Bar Chart" || obj.visualization == "Clustered Horizontal Bar Chart" || obj.visualization == "Stacked Horizontal Bar Chart" || obj.visualization == "Stacked Vertical Bar Chart") {
+
+                            result['dimension'] = metaData.report_line_item.dimension;
+                            result['measure'] = metaData.report_line_item.measure;
+                            result['showXaxis'] = false;
+                            result['showYaxis'] = false;
+                            result['xAxisColor'] = VisualizationUtils.getPropertyValue(obj.property, 'X Axis Colour');
+                            result['yAxisColor'] = VisualizationUtils.getPropertyValue(obj.property, 'Y Axis Colour');
+                            result['showXaxisLabel'] = false;
+                            result['showYaxisLabel'] = false;
+                            result['showLegend'] = false;
+                            result['legendPosition'] = VisualizationUtils.getPropertyValue(obj.property, 'Legend position').toLowerCase();
+                            result['showGrid'] = VisualizationUtils.getPropertyValue(obj.property, 'Show grid');
+
+                            result['displayName'] = metaData.report_line_item.dimension[0];
+
+                            result['showValues'] = [];
+                            result['displayNameForMeasure'] = [];
+                            result['fontStyle'] = [];
+                            result['fontWeight'] = [];
+                            result['fontSize'] = [];
+                            result['numberFormat'] = [];
+                            result['textColor'] = [];
+                            result['displayColor'] = [];
+                            result['borderColor'] = [];
+
+                            for (var i = 0; i < measures.length; i++) {
+
+                                result['showValues'].push(VisualizationUtils.getFieldPropertyValue(measures[i], 'Value on Points'));
+                                result['displayNameForMeasure'].push(
+                                    VisualizationUtils.getFieldPropertyValue(measures[i], 'Display name') ||
+                                    metaData.report_line_item.measure[i]
+                                );
+                                result['fontStyle'].push(VisualizationUtils.getFieldPropertyValue(measures[i], 'Font style'));
+                                result['fontWeight'].push(VisualizationUtils.getFieldPropertyValue(measures[i], 'Font weight'));
+                                result['fontSize'].push(parseInt(VisualizationUtils.getFieldPropertyValue(measures[i], 'Font size')));
+                                result['numberFormat'].push(VisualizationUtils.getFieldPropertyValue(measures[i], 'Number format'));
+                                result['textColor'].push(VisualizationUtils.getFieldPropertyValue(measures[i], 'Text colour'));
+                                var displayColor = VisualizationUtils.getFieldPropertyValue(measures[i], 'Display colour');
+                                result['displayColor'].push((displayColor == null) ? colorSet[i] : displayColor);
+                                var borderColor = VisualizationUtils.getFieldPropertyValue(measures[i], 'Border colour');
+                                result['borderColor'].push((borderColor == null) ? colorSet[i] : borderColor);
+                            }
+                            obj.config = result;
+                            d3.select('#svg_' + obj.visualizationid).html('');
+                            var svg = d3.select('#svg_' + obj.visualizationid)
+
+                            if (obj.visualization == "Clustered Vertical Bar Chart") {
+                                var clusteredverticalbar = flairVisualizations.clusteredverticalbar()
+                                    .config(obj.config)
+                                    .tooltip(true)
+                                    .notification(true)
+                                    .print(false);
+
+                                svg.datum(obj.data)
+                                    .call(clusteredverticalbar);
+                            }
+                            else if (obj.visualization == "Clustered Horizontal Bar Chart") {
+                                var clusteredhorizontalbar = flairVisualizations.clusteredhorizontalbar()
+                                    .config(obj.config)
+                                    .tooltip(true)
+                                    .notification(true)
+                                    .print(false);
+
+                                svg.datum(obj.data)
+                                    .call(clusteredhorizontalbar);
+                            }
+                            else if (obj.visualization == "Stacked Vertical Bar Chart") {
+                                var stackedverticalbar = flairVisualizations.stackedverticalbar()
+                                    .config(obj.config)
+                                    .tooltip(true)
+                                    .notification(true)
+                                    .print(false);
+
+                                svg.datum(obj.data)
+                                    .call(stackedverticalbar);
+                            }
+                            else if (obj.visualization == "Stacked Horizontal Bar Chart") {
+                                var stackedhorizontalbar = flairVisualizations.stackedhorizontalbar()
+                                    .config(obj.config)
+                                    .tooltip(true)
+                                    .notification(true)
+                                    .print(false);
+
+                                svg.datum(obj.data)
+                                    .call(stackedhorizontalbar);
+                            }
+
+                        }
+                        vm.pagedItems.push(obj)
+
+                    },
+                    function (error) { }
+                );
         }
 
         function getScheduledReportsCount() {
--- src/main/webapp/app/services/visualizations/visualization-utils.service.js
+++ src/main/webapp/app/services/visualizations/visualization-utils.service.js
@@ -15,7 +15,8 @@
             getFieldPropertyValue: getFieldPropertyValue,
             getPropertyValue: getPropertyValue,
             getDimensionsAndMeasures: getDimensionsAndMeasures,
-            getFormattedNumber: getFormattedNumber
+            getFormattedNumber: getFormattedNumber,
+            getDimensionsAndMeasuresForNotification:getDimensionsAndMeasuresForNotification
         };
 
         function getFormattedNumber(value, format) {
@@ -53,13 +54,42 @@
             });
 
             var measures = fields.filter(function (item) {
-                    return item.feature && item.feature.featureType === 'MEASURE'
-                }).map(function (item) {
-                    var newItem = {};
-                    angular.copy(item, newItem);
-                    newItem.feature.name = newItem.feature.name.toLowerCase();
-                    return newItem;
-                })
+                return item.feature && item.feature.featureType === 'MEASURE'
+            }).map(function (item) {
+                var newItem = {};
+                angular.copy(item, newItem);
+                newItem.feature.name = newItem.feature.name.toLowerCase();
+                return newItem;
+            })
+                .sort(function (a, b) {
+                    return sortBySequenceNumber(a.fieldType, b.fieldType);
+                });
+            return {
+                measures: measures,
+                dimensions: dimensions
+            };
+        }
+
+        function getDimensionsAndMeasuresForNotification(fields) {
+            var dimensions = fields.filter(function (item) {
+                return item.feature && item.feature.featureType === 'DIMENSION';
+            }).map(function (item) {
+                var newItem = {};
+                angular.copy(item, newItem);
+                newItem.feature.name = newItem.feature.name.toLowerCase();
+                return newItem;
+            }).sort(function (a, b) {
+                return sortBySequenceNumber(a.fieldType, b.fieldType);
+            });
+
+            var measures = fields.filter(function (item) {
+                return item.feature && item.feature.featureType === 'MEASURE'
+            }).map(function (item) {
+                var newItem = {};
+                angular.copy(item, newItem);
+                newItem.feature.name = newItem.feature.name.toLowerCase();
+                return newItem;
+            })
                 .sort(function (a, b) {
                     return sortBySequenceNumber(a.fieldType, b.fieldType);
                 });

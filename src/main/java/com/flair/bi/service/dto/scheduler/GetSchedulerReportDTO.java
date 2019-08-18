217f3a089ff76f8b7527ee3391c53506e3a62a46
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-08-18 13:02:30 +0300

    - added to string for dto

--- src/main/java/com/flair/bi/service/dto/scheduler/GetSchedulerReportDTO.java
+++ src/main/java/com/flair/bi/service/dto/scheduler/GetSchedulerReportDTO.java
@@ -2,9 +2,11 @@ package com.flair.bi.service.dto.scheduler;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @Builder
+@ToString
 public class GetSchedulerReportDTO {
     private final String message;
     private final SchedulerNotificationDTO report;

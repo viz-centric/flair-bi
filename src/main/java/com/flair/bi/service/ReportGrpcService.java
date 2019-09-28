7d411b8756dfa77d93dc0b16fcb7b5acf6141fd3
Sergei Ledvanov <sergei.ledvanov@gmail.com>
2019-09-28 14:59:46 +0300

    remove reports grpc service to avoid listening to 6565 port
    (cherry picked from commit 1a2036a51172b63986dd34c2354f5aa897571c53)

--- src/main/java/com/flair/bi/service/ReportGrpcService.java
+++
@@ -1,9 +0,0 @@
-package com.flair.bi.service;
-
-import org.lognet.springboot.grpc.GRpcService;
-import org.springframework.context.annotation.Profile;
-
-@GRpcService
-@Profile("!test")
-public class ReportGrpcService extends AbstractReportGrpcService {
-}

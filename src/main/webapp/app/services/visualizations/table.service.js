bb360cbf13e13b3f45a631577f13aa5795c309a3
khushbumwa <khushbum.wa@gmail.com>
2019-11-26 20:07:01 +0530

    table chart changes

--- src/main/webapp/app/services/visualizations/table.service.js
+++ src/main/webapp/app/services/visualizations/table.service.js
@@ -86,7 +86,7 @@
                 }
                 function createChart() {
                     $(element[0]).html('')
-                    $(element[0]).append('<div height="' + element[0].clientHeight + '" width="' + element[0].clientWidth + '" style="width:' + element[0].clientWidth + 'px; height:' + element[0].clientHeight + 'px;overflow:auto;text-align:center;position:relative" id="table-' + element[0].id + '" ></div>')
+                    $(element[0]).append('<div height="' + element[0].clientHeight + '" width="' + element[0].clientWidth + '" style="width:' + element[0].clientWidth + 'px; height:' + element[0].clientHeight + 'px;overflow:hidden;text-align:center;position:relative" id="table-' + element[0].id + '" ></div>')
                     var div = $('#table-' + element[0].id)
 
                     var table = flairVisualizations.table()

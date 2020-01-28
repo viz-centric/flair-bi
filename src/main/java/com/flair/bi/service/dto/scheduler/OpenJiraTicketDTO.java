8519387fcfcdd43168c2696d9beffc9569bffd5e
johib.khan@gmail.com <johib.khan@gmail.com>
2020-01-28 19:43:11 +0530

    java api to notify all jira open ticket

---
+++ src/main/java/com/flair/bi/service/dto/scheduler/OpenJiraTicketDTO.java
@@ -0,0 +1,18 @@
+package com.flair.bi.service.dto.scheduler;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class OpenJiraTicketDTO {
+	private String project;
+	private Integer webhookID;
+	private String channels[];
+}

c15626552ff426da3ef7d8ae801e540c1e0e10c7
johib.khan@gmail.com <johib.khan@gmail.com>
2020-04-08 09:49:49 +0530

    1)auto populate/search dimension's data 2) implement tag design and remove comma separated design 3) corrected the alignment of + and - icons after added the tag design icons got disaligned

--- src/main/webapp/app/entities/datasource-constraint/datasource-constraint-dialog.component.js
+++ src/main/webapp/app/entities/datasource-constraint/datasource-constraint-dialog.component.js
@@ -22,7 +22,10 @@
         "Features",
         "$translate",
         "$rootScope",
-        "ComponentDataService"
+        "ComponentDataService",
+        "proxyGrpcService",
+        "AuthServerProvider",
+        "stompClientService"
     ];
 
     function DatasourceConstraintDialogController(
@@ -33,7 +36,10 @@
         Features,
         $translate,
         $rootScope,
-        ComponentDataService
+        ComponentDataService,
+        proxyGrpcService,
+        AuthServerProvider,
+        stompClientService
     ) {
         var vm = this;
 
@@ -44,10 +50,12 @@
         vm.datasourceChange = datasourceChange;
         vm.addConstraint = addConstraint;
         vm.removeConstraint = removeConstraint;
-        vm.createArray = createArray;
         vm.features = [];
         vm.search = search;
-
+        vm.load = load;
+        vm.featureChange=featureChange;
+        vm.added=added;
+        vm.removed=removed;
 
 
         vm.$onInit = function () {
@@ -61,6 +69,7 @@
 
 
         function activate() {
+            connectWebSocket();
             if (
                 vm.datasourceConstraint.constraintDefinition &&
                 vm.datasourceConstraint.constraintDefinition.featureConstraints
@@ -68,15 +77,15 @@
                 vm.datasourceConstraint.constraintDefinition.featureConstraints.forEach(
                     function (item) {
                         if (item.values) {
-                            item.stringValues = item.values.join();
+                            item.selected = item.values.map(function (item) {
+                                var newItem = {};
+                                newItem['text'] = item;
+                                return newItem;
+                            });
                         }
                     }
                 );
             }
-        }
-
-        function createArray(constraint) {
-            constraint.values = constraint.stringValues.split(",");
         }
 
         function addConstraint() {
@@ -179,6 +188,91 @@
                         text: $translate.instant('flairbiApp.datasources.error.datasources.all')
                     });
                 });
+            }
+        }
+
+        function featureChange(type,constraint){
+            constraint.type=type;
+        }
+
+        function load(q,constraint) {
+            var vId = constraint.id;
+            var query = {};
+            query.fields = [{ name: constraint.featureName }];
+            if (q) {
+                query.conditionExpressions = [{
+                    sourceType: 'FILTER',
+                    conditionExpression: {
+                        '@type': 'Like',
+                        featureType: { featureName: constraint.featureName, type: constraint.featureName.type },
+                        caseInsensitive: true,
+                        value: q
+                    }
+                }];
+            }
+            query.distinct = true;
+            query.limit = 20;
+            proxyGrpcService.forwardCall(
+                vm.datasourceConstraint.datasource.id, {
+                queryDTO: query,
+                vId: vId
+            }
+            );
+        }
+        function connectWebSocket() {
+            stompClientService.connect(
+                { token: AuthServerProvider.getToken() },
+                function (frame) {
+                    console.log('flair-bi controller connected web socket');
+                    stompClientService.subscribe("/user/exchange/metaData", onExchangeMetadata);
+                    stompClientService.subscribe("/user/exchange/metaDataError", onExchangeMetadataError);
+                }
+            );
+
+            $scope.$on("$destroy", function (event) {
+                stompClientService.disconnect();
+            });
+        }
+
+        function onExchangeMetadataError(data) {
+            angular.element("#loader-spinner").hide();
+            var body = JSON.parse(data.body || '{}');
+            if (body.description === "io exception") {
+                var msg = $translate.instant('flairbiApp.visualmetadata.errorOnReceivingMataData') + " : " + body.cause.message;
+                $rootScope.showErrorSingleToast({
+                    text: msg
+                });
+            } else {
+                var error = QueryValidationService.getQueryValidationError(body.description);
+                $rootScope.showErrorSingleToast({
+                    text: $translate.instant(error.msgKey, error.params)
+                });
+            }
+        }
+
+        function onExchangeMetadata(data) {
+            var metaData = data.body === "" ? { data: [] } : JSON.parse(data.body);
+            if (data.headers.request === "filters") {
+                $rootScope.$broadcast(
+                    "flairbiApp:filters-meta-Data",
+                    metaData.data
+                );
+            }
+        }
+
+        function added(tag,constraint) {
+            if(constraint.values){
+                constraint.values.push(tag['text']);
+            }else{
+                constraint.values = new Array();
+                constraint.values.push(tag['text']);        
+            }
+        }
+
+        function removed(tag,constraint){
+            var index = constraint.values.indexOf(tag['text']);
+            if (index > -1) {
+                constraint.values.splice(index, 1);
             }
         }
     }

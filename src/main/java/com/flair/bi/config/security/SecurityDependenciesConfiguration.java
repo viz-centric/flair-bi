82ba26f6b1fb06f97f595973cd2fd455fe8be251
admin-vizcentric <admin@vizcentric.com>
2020-05-11 19:29:53 +0200

    Fixed permission issue

--- src/main/java/com/flair/bi/config/security/SecurityDependenciesConfiguration.java
+++ src/main/java/com/flair/bi/config/security/SecurityDependenciesConfiguration.java
@@ -14,8 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 
-import java.util.Collection;
-import java.util.Map;
 import java.util.Optional;
 
 @Configuration

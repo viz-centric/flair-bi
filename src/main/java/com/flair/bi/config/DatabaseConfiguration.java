aa1840c47a990c633973baa4cbde91a613cfc513
admin-vizcentric <admin@vizcentric.com>
2020-05-19 23:17:34 +0200

    Fixed audit date time converter

--- src/main/java/com/flair/bi/config/DatabaseConfiguration.java
+++ src/main/java/com/flair/bi/config/DatabaseConfiguration.java
@@ -1,6 +1,11 @@
 package com.flair.bi.config;
 
+import java.time.OffsetDateTime;
+import java.util.Optional;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -9,10 +14,15 @@ import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableJpaRepositories("com.flair.bi.repository")
-@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware")
+@EnableJpaAuditing(auditorAwareRef = "springSecurityAuditorAware", dateTimeProviderRef = "auditingDateTimeProvider")
 @EnableTransactionManagement
 @RequiredArgsConstructor
 
 public class DatabaseConfiguration {
+
+	@Bean(name = "auditingDateTimeProvider")
+	public DateTimeProvider dateTimeProvider() {
+		return () -> Optional.of(OffsetDateTime.now());
+	}
 
 }

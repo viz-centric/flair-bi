c648f5a31fc24c07b1a5a98a92dc6037ac9dcbaa
sergeil <sergei.ledvanov@gmail.com>
2020-07-03 09:56:58 +0300

    added realm implementation

---
+++ src/main/java/com/flair/bi/domain/Realm.java
@@ -0,0 +1,27 @@
+package com.flair.bi.domain;
+
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "realm")
+@Data
+@EqualsAndHashCode(of = "id")
+public class Realm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+}

4bd651ecc302f701d62c4e9d2aca0d12de8b2334
johib.khan@gmail.com <johib.khan@gmail.com>
2020-07-06 10:22:14 +0530

    created separate constant file of separators list

--- src/test/javascript/karma.conf.js
+++ src/test/javascript/karma.conf.js
@@ -58,6 +58,7 @@ module.exports = function (config) {
             'src/main/webapp/bower_components/stomp-websocket/lib/stomp.min.js',
             'src/main/webapp/bower_components/d3/d3.js',
             'src/main/webapp/bower_components/underscore/underscore.js',
+            'src/main/webapp/bower_components/sql-formatter/dist/sql-formatter.min.js',
             'src/main/webapp/bower_components/angular/angular.js',
             'src/main/webapp/bower_components/angular-aria/angular-aria.js',
             'src/main/webapp/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',

2552e9683b35270b34223b50e4fc6e17ec40f639
sergeil <sergei.ledvanov@gmail.com>
2020-07-21 20:44:01 +0300

    create view on json upload

--- src/main/webapp/app/entities/dashboards/dashboards-overview-content-header.controller.js
+++ src/main/webapp/app/entities/dashboards/dashboards-overview-content-header.controller.js
@@ -8,17 +8,29 @@
     DashboardOverviewContentHeaderController.$inject = [
         "$scope",
         "entity",
-        "AccountDispatch"
+        "AccountDispatch",
+        "Upload",
+        "$state",
+        "$rootScope",
+        "$translate",
+        "$window"
     ];
 
     function DashboardOverviewContentHeaderController(
         $scope,
         entity,
-        AccountDispatch
+        AccountDispatch,
+        Upload,
+        $state,
+        $rootScope,
+        $translate,
+        $window
     ) {
         var vm = this;
 
         vm.selectedDashboard = entity;
+        vm.importedViewFile = null;
+        vm.importView = importView;
 
         activate();
 
@@ -28,6 +40,44 @@
                 "WRITE_" + vm.selectedDashboard.id + "_DASHBOARD"
             );
             
+        }
+
+        function importView(file, errFiles) {
+            vm.importedViewFile = file;
+            if (file) {
+                file.upload = Upload.upload({
+                    url: 'api/dashboards/' + vm.selectedDashboard.id + '/importView',
+                    data: {file: file},
+                    disableProgress: true
+                });
+
+                file.upload.then(function (response) {
+                    $rootScope.showSuccessToast({
+                        text: $translate.instant('flairbiApp.dashboards.upload.success',
+                            {file: vm.importedViewFile.name}),
+                        title: "Uploaded"
+                    });
+                    $window.location.reload();
+                    // $state.reload();
+                }, function (response) {
+                    const errorData = response.data;
+                    if (errorData.message === 'error.viewImportExportError') {
+                        const fieldError = errorData.fieldErrors[0];
+                        $rootScope.showErrorSingleToast({
+                            text: $translate.instant('flairbiApp.dashboards.upload.failure.error.viewImportExportError.' + fieldError.key, {
+                                file: vm.importedViewFile.name,
+                                field: fieldError.field
+                            })
+                        });
+                    } else {
+                        $rootScope.showErrorSingleToast({
+                            text: $translate.instant('flairbiApp.dashboards.upload.failure', {file: vm.importedViewFile.name})
+                        });
+                    }
+                }, function (evt) {
+
+                });
+            }
         }
     }
 })();

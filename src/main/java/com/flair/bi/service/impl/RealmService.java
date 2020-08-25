82257cc4127cfefa7a65aa18115b9a8aff3f37ef
khan-j <johib@flairbi.com>
2020-08-25 10:07:05 +0530

    moved realm dependent code into RealmProcessorService

--- src/main/java/com/flair/bi/service/impl/RealmService.java
+++ src/main/java/com/flair/bi/service/impl/RealmService.java
@@ -1,28 +1,16 @@
 package com.flair.bi.service.impl;
-
-import com.flair.bi.config.Constants;
-import com.flair.bi.domain.Functions;
 import com.flair.bi.domain.Realm;
-import com.flair.bi.domain.User;
-import com.flair.bi.domain.VisualizationColors;
-import com.flair.bi.domain.security.UserGroup;
-import com.flair.bi.repository.FunctionsRepository;
 import com.flair.bi.repository.RealmRepository;
-import com.flair.bi.repository.UserRepository;
-import com.flair.bi.repository.VisualizationColorsRepository;
 import com.flair.bi.service.mapper.RealmMapper;
-import com.flair.bi.service.security.UserGroupService;
 import com.flair.bi.web.rest.dto.RealmDTO;
 import com.querydsl.core.types.Predicate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -31,11 +19,7 @@ public class RealmService {
 
     private final RealmRepository realmRepository;
     private final RealmMapper realmMapper;
-    private final UserRepository userRepository;
-    private final UserGroupService userGroupService;
-    private final PasswordEncoder passwordEncoder;
-    private final FunctionsRepository functionsRepository;
-    private final VisualizationColorsRepository visualizationColorsRepository;
+    private final RealmProcessorService realmProcessorService;
 
 
     @Transactional
@@ -43,12 +27,8 @@ public class RealmService {
         log.debug("Saving realm : {}", realmDTO);
         Realm realm = realmMapper.fromDTO(realmDTO);
         realm = realmRepository.save(realm);
-        // TODO create user group service is not done yet
-        //userRepository.save(createUser(realm));
-        //userGroupService.save(createUserGroup(realm));
         Realm realmV = realmRepository.findByName("vizcentric");
-        functionsRepository.saveAll(buildFunctionsList(realm,realmV.getId()));
-        visualizationColorsRepository.saveAll(buildVisualizationColorsList(realm,realmV.getId()));
+        realmProcessorService.saveRealmDependentRecords(realm,realmV.getId());
         return realmMapper.toDTO(realm);
     }
 
@@ -76,61 +56,7 @@ public class RealmService {
     @Transactional
     public void delete(Long id) {
         log.debug("Request to delete Functions : {}", id);
-        // TODO
-        visualizationColorsRepository.deleteByRealmId(id);
-        functionsRepository.deleteByRealmId(id);
+        realmProcessorService.deleteRealmDependentRecords(id);
         realmRepository.deleteById(id);
-    }
-
-    private User createUser(Realm realm){
-        User newUser = new User();
-        newUser.setLogin(realm.getName().trim()+realm.getId());
-        String encryptedPassword = passwordEncoder.encode(realm.getName().trim()+realm.getId()+"@123");
-        newUser.setPassword(encryptedPassword);
-        newUser.setFirstName(realm.getName());
-        newUser.setLastName("Administrator");
-        newUser.setEmail(realm.getName().trim()+realm.getId()+"@localhost");
-        newUser.setLangKey(Constants.LanguageKeys.ENGLISH);
-        newUser.setActivated(true);
-        newUser.setCreatedBy("system");
-        newUser.setLastModifiedBy("system");
-        newUser.setRealm(realm);
-        return newUser;
-    }
-
-    private UserGroup createUserGroup(Realm realm){
-        UserGroup userGroup = new UserGroup();
-        userGroup.setName("ROLE_ADMIN");
-        userGroup.setRealm(realm);
-        return userGroup;
-    }
-
-    private List<Functions> buildFunctionsList(Realm realm,Long realmIdV){
-        List<Functions> functions = functionsRepository.findByRealmId(realmIdV)
-                .stream()
-                .map(f -> {
-                    Functions function = new Functions();
-                    function.setDescription(f.getDescription());
-                    function.dimensionUse(f.isDimensionUse());
-                    function.measureUse(f.isMeasureUse());
-                    function.setName(f.getName());
-                    function.setValidation(f.getValidation());
-                    function.setValue(f.getValue());
-                    function.setRealm(realm);
-                    return function;
-                }).collect(Collectors.toList());
-        return functions;
-    }
-
-    private List<VisualizationColors> buildVisualizationColorsList(Realm realm,Long realmIdV){
-        List<VisualizationColors> visualizationColors = visualizationColorsRepository.findByRealmId(realmIdV)
-                .stream()
-                .map(v ->{
-                    VisualizationColors visualizationColor = new VisualizationColors();
-                    visualizationColor.setCode(v.getCode());
-                    visualizationColor.setRealm(realm);
-                    return visualizationColor;
-                }).collect(Collectors.toList());
-        return visualizationColors;
     }
 }

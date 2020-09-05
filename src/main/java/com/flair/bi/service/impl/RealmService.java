947011e2460f55e7e923c09e6709fe1c9a099b1d
ir73-flair <013914@gmail.com>
2020-09-05 21:54:35 +0300

    changed base realm that is used as a source

--- src/main/java/com/flair/bi/service/impl/RealmService.java
+++ src/main/java/com/flair/bi/service/impl/RealmService.java
@@ -1,6 +1,10 @@
 package com.flair.bi.service.impl;
+
 import com.flair.bi.domain.Realm;
+import com.flair.bi.domain.User;
 import com.flair.bi.repository.RealmRepository;
+import com.flair.bi.security.SecurityUtils;
+import com.flair.bi.service.UserService;
 import com.flair.bi.service.mapper.RealmMapper;
 import com.flair.bi.web.rest.dto.RealmDTO;
 import com.querydsl.core.types.Predicate;
@@ -10,6 +14,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -20,15 +25,15 @@ public class RealmService {
     private final RealmRepository realmRepository;
     private final RealmMapper realmMapper;
     private final RealmProcessorService realmProcessorService;
-
+    private final UserService userService;
 
     @Transactional
     public RealmDTO save(RealmDTO realmDTO) {
         log.debug("Saving realm : {}", realmDTO);
         Realm realm = realmMapper.fromDTO(realmDTO);
         realm = realmRepository.save(realm);
-        Realm realmV = realmRepository.findByName("vizcentric");
-        realmProcessorService.saveRealmDependentRecords(realm,realmV.getId());
+        User user = userService.getUserWithAuthoritiesByLoginOrError(SecurityUtils.getCurrentUserLogin());
+        realmProcessorService.saveRealmDependentRecords(realm, user.getRealm().getId());
         return realmMapper.toDTO(realm);
     }
 

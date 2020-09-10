a7c6bdb06c942ff562cff94490ad8b5ed786fc00
ir73-flair <013914@gmail.com>
2020-09-10 08:20:40 +0300

    change view endpoint protection

--- src/main/java/com/flair/bi/repository/ViewRepository.java
+++ src/main/java/com/flair/bi/repository/ViewRepository.java
@@ -1,7 +1,9 @@
 package com.flair.bi.repository;
 
-import java.util.stream.Stream;
-
+import com.flair.bi.domain.QView;
+import com.flair.bi.domain.View;
+import com.querydsl.core.types.dsl.SimpleExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,10 +13,7 @@ import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.flair.bi.domain.QView;
-import com.flair.bi.domain.View;
-import com.querydsl.core.types.dsl.SimpleExpression;
-import com.querydsl.core.types.dsl.StringExpression;
+import java.util.stream.Stream;
 
 /**
  * Spring Data JPA repository for the View entity.
@@ -23,8 +22,9 @@ import com.querydsl.core.types.dsl.StringExpression;
 public interface ViewRepository
 		extends JpaRepository<View, Long>, QuerydslPredicateExecutor<View>, QuerydslBinderCustomizer<QView> {
 
-	@Query(value = "select views from View views where views.viewDashboard.id = :id")
-	Stream<View> findByDashboardId(@Param("id") Long id);
+	Stream<View> findAllByViewDashboardId(Long id);
+
+	Stream<View> findAllByViewDashboardIdAndRealmId(Long id, Long realmId);
 
 	@Query(value = "select view from View view where view.viewDashboard.id = :id and view.viewName=:viewName")
 	View findByDashboardIdAndViewName(@Param("id") Long id, @Param("viewName") String viewName);

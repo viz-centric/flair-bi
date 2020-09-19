698b384f7321b01af858ccd79c6962e2d285b4c3
ir73-flair <013914@gmail.com>
2020-09-19 21:09:56 +0300

    added realm checks to features

--- src/main/java/com/flair/bi/service/ViewFeatureCriteriaService.java
+++ src/main/java/com/flair/bi/service/ViewFeatureCriteriaService.java
@@ -3,7 +3,6 @@ package com.flair.bi.service;
 import com.flair.bi.domain.Feature;
 import com.flair.bi.domain.View;
 import com.flair.bi.domain.ViewFeatureCriteria;
-import com.flair.bi.repository.FeatureRepository;
 import com.flair.bi.view.ViewService;
 import com.flair.bi.web.rest.dto.CreateViewFeatureCriteriaRequest;
 import lombok.RequiredArgsConstructor;
@@ -18,34 +17,34 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ViewFeatureCriteriaService {
 
-	private final FeatureRepository featureRepository;
+    private final FeatureService featureService;
 
-	private final ViewService viewService;
+    private final ViewService viewService;
 
-	public Set<ViewFeatureCriteria> create(CreateViewFeatureCriteriaRequest request) {
-		View view = viewService.findOne(request.getViewId());
+    public Set<ViewFeatureCriteria> create(CreateViewFeatureCriteriaRequest request) {
+        View view = viewService.findOne(request.getViewId());
 
-    final Set<ViewFeatureCriteria> viewFeatureCriterias = request.getFeatures().stream()
-				.map(f -> composeViewFeatureCriteria(view, f)).collect(Collectors.toSet());
-    // remove existing one
-    view.getViewFeatureCriterias().forEach(view::remove);
+        final Set<ViewFeatureCriteria> viewFeatureCriterias = request.getFeatures().stream()
+                .map(f -> composeViewFeatureCriteria(view, f)).collect(Collectors.toSet());
+        // remove existing one
+        view.getViewFeatureCriterias().forEach(view::remove);
 
-    //add new one
-    viewFeatureCriterias.forEach(view::add);
+        //add new one
+        viewFeatureCriterias.forEach(view::add);
 
-		viewService.save(view);
+        viewService.save(view);
 
-		return view.getViewFeatureCriterias();
-	}
+        return view.getViewFeatureCriterias();
+    }
 
-	private ViewFeatureCriteria composeViewFeatureCriteria(View view,
-			CreateViewFeatureCriteriaRequest.ViewFeatureData f) {
-		Feature feature = featureRepository.getOne(f.getFeatureId());
-		ViewFeatureCriteria viewFeatureCriteria = new ViewFeatureCriteria();
-		viewFeatureCriteria.setFeature(feature);
-		viewFeatureCriteria.setView(view);
-		viewFeatureCriteria.setValue(f.getValue());
-		viewFeatureCriteria.setMetadata(f.getMetadata());
-		return viewFeatureCriteria;
-	}
+    private ViewFeatureCriteria composeViewFeatureCriteria(View view,
+                                                           CreateViewFeatureCriteriaRequest.ViewFeatureData f) {
+        Feature feature = featureService.getOne(f.getFeatureId());
+        ViewFeatureCriteria viewFeatureCriteria = new ViewFeatureCriteria();
+        viewFeatureCriteria.setFeature(feature);
+        viewFeatureCriteria.setView(view);
+        viewFeatureCriteria.setValue(f.getValue());
+        viewFeatureCriteria.setMetadata(f.getMetadata());
+        return viewFeatureCriteria;
+    }
 }

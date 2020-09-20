90d584acc2d802bab8cda6a2f230515cad2ea8bb
ir73-flair <013914@gmail.com>
2020-09-20 12:53:32 +0300

    added realm checks to field types

--- src/main/java/com/flair/bi/repository/FieldTypeRepository.java
+++ src/main/java/com/flair/bi/repository/FieldTypeRepository.java
@@ -1,14 +1,9 @@
 package com.flair.bi.repository;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.flair.bi.domain.fieldtype.FieldType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface FieldTypeRepository extends JpaRepository<FieldType, Long> {
+public interface FieldTypeRepository extends JpaRepository<FieldType, Long>, QuerydslPredicateExecutor<FieldType> {
 
-	Page<FieldType> findByVisualizationId(Long visualizationId, Pageable pageable);
-
-	FieldType findByIdAndVisualizationId(Long id, Long visualizationsId);
 }

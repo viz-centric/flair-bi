0e4f2ff340b8a6fe16af51de9c8a040373575d1e
ir73-flair <013914@gmail.com>
2020-10-25 13:01:34 +0200

    removed unused code

--- src/main/java/com/flair/bi/domain/constraintdefinition/ConstraintGroupDefinition.java
+++ src/main/java/com/flair/bi/domain/constraintdefinition/ConstraintGroupDefinition.java
@@ -1,19 +1,11 @@
 package com.flair.bi.domain.constraintdefinition;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@EqualsAndHashCode(of = "featureConstraints")
+@Data
 public class ConstraintGroupDefinition  {
 
 	private List<ConstraintGroupExpression> featureConstraints = new ArrayList<>();

6176fe68a4c7ab77f429f5ec4937e2336cb34889
Flair Viz Dev <flairvizentricdev17@gmail.com>
2020-12-29 17:22:19 +0200

    draft user

---
+++ src/main/java/com/flair/bi/domain/DraftUser.java
@@ -0,0 +1,62 @@
+package com.flair.bi.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.flair.bi.config.Constants;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Entity
+@Table(name = "draft_user")
+@EqualsAndHashCode(of = "username", callSuper = false)
+@Data
+public class DraftUser extends AbstractAuditingEntity implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@NotNull
+	@Pattern(regexp = Constants.LOGIN_REGEX)
+	@Size(min = 1, max = 50)
+	@Column(length = 50, unique = true, nullable = false)
+	private String username;
+
+	@JsonIgnore
+	@NotNull
+	@Size(min = 60, max = 60)
+	@Column(name = "password_hash", length = 60)
+	private String password;
+
+	@Size(max = 50)
+	@Column(name = "first_name", length = 50)
+	private String firstName;
+
+	@Size(max = 50)
+	@Column(name = "last_name", length = 50)
+	private String lastName;
+
+	@Email
+	@Size(max = 100)
+	@Column(length = 100, unique = true)
+	private String email;
+
+	@Column(name = "date_created")
+	private Instant dateCreated = null;
+
+	@Size(max = 20)
+	@Column(name = "user_type", length = 20)
+	private String userType;
+}
---
+++ src/main/java/com/flair/bi/service/DraftUserService.java
@@ -0,0 +1,38 @@
+package com.flair.bi.service;
+
+import com.flair.bi.config.Constants;
+import com.flair.bi.domain.DraftUser;
+import com.flair.bi.repository.DraftUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class DraftUserService {
+
+	private final PasswordEncoder passwordEncoder;
+	private final DraftUserRepository draftUserRepository;
+
+	public DraftUser createUser(String login, String password, String firstName, String lastName, String email,
+								String userType) {
+		DraftUser user = new DraftUser();
+		String encryptedPassword = passwordEncoder.encode(password);
+		user.setUsername(login);
+		user.setPassword(encryptedPassword);
+		user.setFirstName(firstName);
+		user.setLastName(lastName);
+		user.setEmail(email);
+		user.setUserType(StringUtils.isNoneBlank(userType) ? userType : Constants.INTERNAL_USER);
+		user.setDateCreated(Instant.now());
+		draftUserRepository.save(user);
+		log.debug("Creating draft user {}", user);
+		return user;
+	}
+
+}
